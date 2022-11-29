a4e8b0b058cf4cd70890e52ab09a7f34eca0081f
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2022-11-29 12:16:45 +0530

    Add files via upload

---
+++ Addition.c
@@ -0,0 +1,29 @@
+// #include <stdio.h>
+
+// int main()
+// {
+//    int a = 12;
+//    int b = 23;
+//    int c = a + b ;
+
+//    printf("sum of two number is %d",c);
+
+//     return 0;
+// }
+
+#include<stdio.h>
+
+int main()
+{
+    int a,b;
+   // int Sum = 0;
+    printf("Enter a value a \n");
+    scanf("%d",&a);
+
+    printf("Enter a value of b \n");
+    scanf("%d", &b);
+
+  //  Sum = a +b;
+
+    printf("The sum of a And b is %d", a + b);
+}
