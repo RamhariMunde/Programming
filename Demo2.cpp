7126f7c5435675066e075627d37cf56658161b27
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2022-12-01 23:09:58 +0530

    Time complexity

---
+++ Demo2.cpp
@@ -0,0 +1,26 @@
+// time comlexity Analysi in cpp
+#include<iostream>
+using namespace std;
+
+int sum_in_range(int x, int y){
+    int result = 0;
+    for (int i = x; i <= y; i++ ){
+        result += i;
+        
+    }
+    return result;
+}
+// this ia another method to solve complexity 
+int sum_in_range_optimised(int x, int y){
+    int n = (y - x + 1);
+    int a = x;
+    int result = (n * (2*a + (n-1)*1))/ 2;
+    return result;
+}
+
+int main()
+{
+    cout<< sum_in_range_optimised(1,100000);
+  
+    return 0;
+}
