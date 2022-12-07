4fedcbfc8ebaa3d46c6028cf1b5db1f69dc8dfac
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2022-12-07 18:13:25 +0530

    Spiral shape python

---
+++ Spiral_shape.py
@@ -0,0 +1,15 @@
+import turtle as t 
+
+space=1
+range=400
+colourlist=['purpal','red','orange','yellow','blue']
+
+t.setup(width = 600 ,height=600)
+t.setup(range/10+1)
+t.forward(space)
+t.left(120)
+t.left(1)
+space+=1
+range+=1
+
+t.done()
