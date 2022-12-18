49efd192851719b4769af0f5bf2084cbb0c73566
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2022-12-18 23:09:41 +0530

    emoji pattern

---
+++ Pattern2.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+    int rows;
+
+    printf("Enter no of rows :");
+    scanf("%d", &rows);
+
+    for (int i=1; i<=rows;i++)
+    {
+        for (int space=1; space<=rows-i; space++)
+        {
+            printf(" ");
+        }
+     for (int j=1; j<=i; j++)
+     {
+        printf("💃");
+     }  
+     printf("\n"); 
+    }
+    return 0;
+}
