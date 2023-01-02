b2f3cf3353162e27504dcbb237d6141ef05f1e8b
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-02 11:49:12 +0530

    Add files via upload

---
+++ Control.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+
+int main()
+// {
+
+//     printf("%d\n",4 == 4); // equal 1,0
+//     return 0;
+
+// }
+
+// {
+//     printf("%d", 3 <= 4);
+//     return 0 ;
+// }
+
+// {
+//     printf("%d", 3 != 4);
+//     return 0;
+// }
+
+// Logical Operators &&, !, ||
+
+// {
+//     printf("%d",!(5>1) && (3>5));
+//     return 0;
+
+// }
+
+// {
+//     int x;
+//     printf("enter a number :");
+//     scanf("d",&x);
+//     printf("%d", x % 2 == 0);
+//     return 0;
+// }
+
+// {
+//     int isSunday = 1;
+//     int isSnowing = 1;
+//     printf("%d \n", isSunday && isSnowing);
+//     return 0;
+// }
+{
+    int x;
+    printf("enter number : ");
+    scanf("%d",&x);
+    printf("%d \n", x>9 && x< 100);
+    return 0;
+}
+
+// int main()
+// {
+//     int age = 22;
+//     printf("my age is %d",age);
+//     return 0;
+
+// }
+
+// int main()
+// {
+//     int age;
+//     printf("Enter your age ");
+//     scanf("%d",&age);
+//     printf("age is: %d",age); // %d Format specifiers
+
+//     return 0;
+// }
+
+// int main()
+// {
+//     int a,b;
+//     printf("Enter a");
+//     scanf("%d",&a);
+
+//     printf("Enter b");
+//     scanf("%d",&b);
+
+//     printf("sum of a and b is %d",a+b);
+//     return 0;
+// }
+
+// int main()
+// {
+//     int side;
+//     printf("Enter a side");
+//     scanf("%d",&side);
+
+//     printf("area is :%d", side * side);
+//     return 0;
+// }
+
+// int main()
+// {
+//     float radius;
+//     printf("Enter radius of Circal");
+//     scanf("%f",&radius);
+
+//     printf("radius of circal is : %f", 3.14 * radius * radius);
+//     return 0;
+   
+// }
