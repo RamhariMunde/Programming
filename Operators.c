40daaee21db6d2c80d8d00123d7d462e1b033b92
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-03 08:41:23 +0530

    Add files via upload

---
+++ Operators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+// int main()
+// {
+//     int x,y,z;
+//     x = y = z = 1;
+//     printf("value is : %d", x);
+//      return 0;
+// }
+
+
+// int main()
+// {
+//     //int ram = pow(2,3);
+//     printf("value %d",-8%3);
+//     return 0;
+
+// }
+
+int main()
+{
+    int a = 5*(2 / 2)*3;
+    printf("%d",a);
+    return 0;
+}
