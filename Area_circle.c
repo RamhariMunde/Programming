6e727e6a8bf9c3534c8066746c09f7ed612120fc
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2023-01-11 00:05:01 +0530

    Area of circle

---
+++ Area_circle.c
@@ -0,0 +1,13 @@
+// write a program to calculate area of a circle
+
+#include<stdio.h>
+
+int main()
+{
+    float radius;
+    printf("Enter radius ");
+    scanf("%f",&radius);
+
+    printf("area is : %f",3.14 * radius * radius);
+    return 0;
+}
